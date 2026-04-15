b1c974130c0473c011de597ca1d65105a06e19b6
boxblackg-tech <boxblack.g@gmail.com>
2026-04-15 20:31:12 +0700

    Update sheetConfig.js

--- js/sheetConfig.js
+++ js/sheetConfig.js
@@ -1,6 +1,6 @@
 export const SHEET_BACKEND = {
   spreadsheetId: "1aueJX_coe3TeHoBLwge5Ww9C15T5lg8MlBmfLrLj_T8",
-  webAppUrl: ""
+  webAppUrl: "https://script.google.com/macros/s/AKfycbz4967JT5PJveC9cMJOoyRGsvMitjOGv47n0Coy92PHaCb-OSEG34ThOI1cxeL4sGp_/exec"
 };
 
 export function getSheetBackendConfig() {
